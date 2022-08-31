53498c7a7ce7863012cbb7d023012ec5652d57e4
Nabamita Pyne <71277217+nabamita-pyne@users.noreply.github.com>
2022-09-01 01:53:20 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","D://Selenium//Drivers//chromeDrivers/chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+
+        //Navigate the driver
+        driver.get("https://www.google.com");
+
+        driver.close();
+
+    }
+}
